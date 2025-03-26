49a30bca76f8a7ac6fda3d35c82d445837e03020
Kartikey-Soni <kartikeysoni06@gmail.com>
2025-03-26 22:18:21 +0530

    Update health.py

--- health.py
+++ health.py
@@ -47,7 +47,7 @@ uploaded_file = st.file_uploader(
 image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_container_width=True)
 
 
 submit = st.button("Tell me the about the food")
